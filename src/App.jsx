e6e508e1918bdedc0dc888f5a0cfa162636c01a0
angelica <guidolinangelica66@gmail.com>
2025-02-06 12:36:08 +0100

    feat add list with the post

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,35 @@
-export default function App(){}
+import { useState, useEffect } from "react";
+import axios from "axios"
+const postPrev= {
+    id: 1,
+      title: "",
+      content:"",
+      image: "",
+      tags: [],
+}
+export default function App(){
+    const [postList, setPostList] = useState([])
+
+
+    const fetchPost=() =>{
+        axios.get('http://localhost:3000/post')
+        .then(function (response) {
+            setPostList(response.data)
+        })
+      
+    }
+    postEff(fetchPost,[]);
+  return(
+    <>
+    <h1>Post ricette</h1>
+    <div>
+        <ul>
+            {postList.map ((post)=>
+                <li key={post.id}>{post.title}</li> 
+            )}
+            
+        </ul>
+    </div>
+    </>
+  )
+}
